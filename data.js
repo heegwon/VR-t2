a3f9c72b8f2225efdf72697a553637460981925f
heegwon <heegwon0301@gmail.com>
2024-10-16 11:15:20 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,228 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3",
+      "name": "파노라마3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.45593127492457874,
+          "pitch": 0.06892033231263639,
+          "rotation": 0,
+          "target": "1-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6540644375499731,
+          "pitch": -0.32317973857642457,
+          "title": "상가 전면",
+          "text": "XXX 상가의 정면 디자인<div><br><div>-심플함, 노멀함 을 강조한 파사트</div><div>-주간 및 야간 어디서나 접근이 용함</div><div><div>심플함, 노멀함 을 강조한 파사트</div><div>-주간 및 야간 어디서나 접근이 용함</div><div><div>심플함, 노멀함 을 강조한 파사트</div><div>-주간 및 야간 어디서나 접근이 용함</div><div><div>심플함, 노멀함 을 강조한 파사트</div><div>-주간 및 야간 어디서나 접근이 용함</div><div><br></div></div></div></div></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-5",
+      "name": "파노라마5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.13534193744206746,
+          "pitch": 0.046706572619960696,
+          "rotation": 0,
+          "target": "2-6"
+        },
+        {
+          "yaw": -1.662767211036046,
+          "pitch": -0.06567627050597125,
+          "rotation": 0,
+          "target": "0-3"
+        },
+        {
+          "yaw": 2.074216070714577,
+          "pitch": -0.17728297626089073,
+          "rotation": 0,
+          "target": "5-9"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5536896288066444,
+          "pitch": -0.07396352956139651,
+          "title": "옥외 접근 경로 A",
+          "text": "A방향 접근로"
+        },
+        {
+          "yaw": 2.1938948002252197,
+          "pitch": -0.17119561876398315,
+          "title": "B방향 접근로",
+          "text": "B방향에서 접근하는 경로"
+        }
+      ]
+    },
+    {
+      "id": "2-6",
+      "name": "파노라마6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 1.6882799987421695,
+        "pitch": 0.18239528350366285,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0189335998694915,
+          "pitch": 0.03899200001207426,
+          "rotation": 0,
+          "target": "4-8"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.147507626425292,
+          "pitch": 0.03110709690775515,
+          "title": "카운터",
+          "text": "-접객이 이루어지는 공간<div>-주문이 가능한 영역</div><div>-뒤쪽에 주방이 위치</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-7",
+      "name": "파노라마7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-8",
+      "name": "파노라마8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.011797382507467091,
+        "pitch": 0,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.4551014758408538,
+          "pitch": -0.25600026610409365,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.3097310765385668,
+          "pitch": 0.25355662071482854,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5791966817947998,
+          "pitch": -0.07662693012773047,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-9",
+      "name": "파노라마9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
